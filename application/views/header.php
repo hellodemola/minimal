ac90cf7bff1d9549fa19d93cd68f98f39a060336
DESKTOP-9BTUH62\Ade <ademola.onasoga@gmail.com>
2018-09-16 13:10:30 +0100

    theme color with social media meta data

--- application/views/header.php
+++ application/views/header.php
@@ -21,4 +21,11 @@
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>assets/css/main.css" />
     <link href="https://fonts.googleapis.com/css?family=Sacramento|Roboto:100" rel="stylesheet"> 
     <link rel="stylesheet" type="text/css" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"/>
+
+    <meta name="description" content="Web developer that develop relevant contents that keeps you found, with ease. Hello demola">
+    <meta name="title" content="Hello demola">
+    <meta name="keywords" content="webdesign, coding, socialmedia, profession designer, lagos, Nigeria, ">
+    <meta name="author" content="hellodemola.com">
+    <meta property="og:image" content="<?php echo base_url(); ?>assets/images/og.png" />
+
 </head>
